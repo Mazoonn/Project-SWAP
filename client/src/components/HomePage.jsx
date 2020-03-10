dd3c09380634f88425e41d4c727d5dd20cec8926
Matan Nunberg <nunberg20@gmail.com>
2020-03-10 18:47:16 +0200

    update and tasting of client login to system for admin

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  state = {};
+  render() {
+    return <div>Welcome to the Home Page!</div>;
+  }
+}
+
+export default HomePage;
