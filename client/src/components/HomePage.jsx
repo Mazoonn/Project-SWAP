ab14f556681ded543b3c73fb0c3765ad5cc68768
Matan Nunberg <nunberg20@gmail.com>
2020-03-21 21:50:30 +0200

    Adding Google Map
    adding google map and funding the right modle to find location

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
-
+import SimpleMap from "./SimpleMap";
+// import Location from "./locationTest";
 class HomePage extends Component {
   state = {};
   render() {
-    return <div>Welcome to the Home Page!</div>;
+    return (
+      <React.Fragment>
+        {/* . <Location /> */}
+        <div>Welcome to the Home Page!</div>
+        <SimpleMap></SimpleMap>
+      </React.Fragment>
+    );
   }
 }
 
