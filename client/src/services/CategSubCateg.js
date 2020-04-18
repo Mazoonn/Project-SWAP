7b006a98011a54f286243fe1c3b6e38791b13613
SlavikMironov <61431810+SlavikMironov@users.noreply.github.com>
2020-04-18 03:14:04 +0300

    More types

--- client/src/services/CategSubCateg.js
+++ client/src/services/CategSubCateg.js
@@ -5,6 +5,12 @@ const CategorySubCategory = [
   { id1: 2, id2: 1, name: "Cafe" },
   { id1: 2, id2: 2, name: "Beer Garden" },
   { id1: 2, id2: 3, name: "Dance Bar" },
+  { id1: 3, id2: 1, name: "Art" },
+  { id1: 3, id2: 2, name: "Biographical" },
+  { id1: 3, id2: 3, name: "Automobile" },
+  { id1: 3, id2: 4, name: "Children's" },
+  { id1: 4, id2: 2, name: "Slots" },
+  { id1: 4, id2: 3, name: "Video Poker" },
 ];
 
 export function getSubCategoriesId(id) {
--- client/src/services/Categories.js
+++ client/src/services/Categories.js
@@ -1,6 +1,8 @@
 const categorys = [
   { id: 1, name: "Restaurants" },
   { id: 2, name: "Bars" },
+  { id: 3, name: "Museum" },
+  { id: 4, name: "Casino" },
 ];
 export function getCategories() {
   return categorys;
